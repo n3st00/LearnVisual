08a651d8477e5c3d0d079b46cf8223192fda2e15
n3st00 <119899114+n3st00@users.noreply.github.com>
2024-03-01 07:56:51 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LearnVisual
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
