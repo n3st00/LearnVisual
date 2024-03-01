811e25ca4f0b3effde8b818ae036241f31faf445
n3st00 <119899114+n3st00@users.noreply.github.com>
2024-03-01 15:19:06 +0100

    Added Logo Window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,18 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LearnVisual.UserControls; 
+
 
 namespace LearnVisual
 {
@@ -19,6 +24,22 @@ namespace LearnVisual
         public MainWindow()
         {
             InitializeComponent();
+            LogoWindow logoWindowInstance = new LogoWindow();
+            LoadUserControl(logoWindowInstance);
+            logoWindowInstance.LogoButtonClicked += HandleLogoWindowEvent;
+
         }
+
+        private async Task HandleLogoWindowEvent(object sender, EventArgs e) 
+        {
+            Content = null;
+
+        }
+        private void LoadUserControl(UserControl userControl)
+        {
+            Content = userControl;
+
+        }
+
     }
 }
---
+++ UserControls/LogoWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnVisual.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LogoWindow.xaml
+    /// </summary>
+    public partial class LogoWindow : UserControl
+    {
+        public delegate Task LogoButtonClickedEventHandler(object sender, EventArgs e);
+        public event LogoButtonClickedEventHandler LogoButtonClicked;
+
+        public LogoWindow()
+        {
+            InitializeComponent();
+        }
+
+
+       
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the storyboard from resources
+            var storyboard = Resources["DisappearAnimation"] as Storyboard;
+            // Begin the animation
+            storyboard?.Begin();
+            // Wait for the animation to complete
+            await Task.Delay(1000); // Adjust the delay based on the animation duration
+
+            LogoButtonClicked.Invoke(this, EventArgs.Empty);
+
+        }
+    }
+}
